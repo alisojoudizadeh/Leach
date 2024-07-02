2e36040eb55cb55c682bc608690f12aa56e68fe5
ali sojoudizadeh <49396867+alisojoudizadeh@users.noreply.github.com>
2024-07-02 10:48:25 +0330

    Update prob.m

--- Cluster/prob.m
+++ Cluster/prob.m
@@ -5,9 +5,7 @@ function P = prob(r, p)
 %       p     p?
 %   Example:
 %       P = prob(1, 0.1);
-%
-% Mohammad Hossein Homaei, homaee@gmail.com, Homaei@wsnlab.org
-% Ver 2. 06/2014
+
 %
     if ~exist('p','var')
         p = 0.1;
